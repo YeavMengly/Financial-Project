31c23921cf81d5f0469240d295438d3a257bf464
YeavMengly <yeavmengly@gmail.com>
2026-03-18 12:35:55 +0700

    update version

--- app/DataTables/Water/InitialWaterEntityDataTable.php
+++ app/DataTables/Water/InitialWaterEntityDataTable.php
@@ -2,10 +2,12 @@
 
 namespace App\DataTables\Water;
 
-use App\Models\BeginCredit\Ministry;
+use App\Models\Content\Ministry;
 use App\Models\InitialWaterEntity;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
+use Illuminate\Http\Request;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
@@ -29,27 +31,63 @@ class InitialWaterEntityDataTable extends DataTable
                     ? '<span class="badge bg-success">' . __('buttons.active') . '</span>'
                     : '<span class="badge bg-danger">' . __('buttons.deleted') . '</span>';
             })
+            ->editColumn('is_archived', function ($module) {
+                $notes = ($module->is_archived == 2) ? '<button class="btn btn-sm btn-outline-success">បានបញ្ចប់</button>' : '<button class="btn btn-sm btn-outline-primary">កំពុងធ្វើ</button>';
+
+                return $notes;
+            })
+            ->addColumn("dateTime", function ($module) {
+                return Carbon::parse($module->created_at)->format('Y-m-d  h:i:s A');
+            })
             ->addColumn('action', function ($model) {
                 return view('water::water.entity.initialWaterEntity.action', ['module' => $model]);
             })
-            ->rawColumns(['soft_delete', 'action']);
+            ->rawColumns(['status', 'soft_delete', 'action', 'is_archived']);
     }
 
     /**
      * Get the query source of dataTable.
      */
-    public function query(Ministry $model): QueryBuilder
+    public function query(Ministry $model,  Request $request): QueryBuilder
     {
-        $query = $model->newQuery()->select([
+        $model = $model->newQuery();
+
+        if ($request->cboStatus) {
+            if ($request->cboStatus == '2') {
+                $model->where('ministries.deleted_at', null);
+            } elseif ($request->cboStatus == '3') {
+                $model->where('ministries.deleted_at', '!=', null);
+            } else {
+                $model->withTrashed();
+            }
+        } else {
+            $model->where('ministries.deleted_at', null);
+        }
+
+        if ($request->cboTodo) {
+            if ($request->cboTodo == 2) {
+                $model->where('ministries.is_archived', 1);
+            } elseif ($request->cboTodo == 3) {
+                $model->where('ministries.is_archived', 2);
+            }
+        } else {
+            $model->where('ministries.is_archived', 1);
+        }
+
+        $model->select([
             'ministries.id',
             'ministries.no',
             'ministries.year',
             'ministries.title',
             'ministries.refer',
-            'ministries.name'
+            'ministries.name',
+            'ministries.status',
+            'ministries.is_archived',
+            'ministries.created_at',
+            'ministries.deleted_at'
         ]);
 
-        return $query->orderBy('ministries.id', 'ASC');
+        return $model->orderBy('ministries.id', 'DESC');
     }
 
     /**
@@ -64,7 +102,18 @@ class InitialWaterEntityDataTable extends DataTable
                 'language' => [
                     'url' => asset('assets/lang/language.json'),
                 ],
+            ])->ajax([
+                'data' => 'function(d) {
+                    d.cboTodo = $("#cboTodo").val();
+                    d.cboStatus = $("#cboStatus").val();
+                }',
             ])
+            ->initComplete('function () {
+                $("#filter").submit(function(event) {
+                    event.preventDefault();
+                    $("#initialwaterentity-table").DataTable().ajax.reload();
+                });
+            }')
             ->orderBy(2, 'ASC');
     }
 
@@ -78,11 +127,14 @@ class InitialWaterEntityDataTable extends DataTable
                 'DT_RowIndex',
                 __('tables.th.no')
             )->width(30)->addClass('text-center align-middle')->orderable(false),
+            Column::computed('is_archived')->title(__('Task'))->width(100)->addClass('text-center align-middle'),
 
             Column::make('year')->title(__('tables.th.year'))->width(80)->addClass('align-middle'),
-
+            Column::make('title')->title(__('tables.th.title'))->addClass('align-middle'),
+            Column::make('refer')->title(__('tables.th.refer'))->addClass('align-middle'),
             Column::make('name')->title(__('tables.th.description'))->addClass('align-middle'),
 
+            Column::computed('soft_delete')->title(__('tables.th.status'))->width(100)->addClass('text-center'),
             Column::computed(
                 'action',
                 __('tables.th.action')
