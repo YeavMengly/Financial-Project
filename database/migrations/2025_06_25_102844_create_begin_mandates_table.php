4d3c4826f86e957f59eced48e58c59640b2fec6a
YeavMengly <yeavmengly@gmail.com>
2026-03-20 12:01:26 +0700

    update interface

--- database/migrations/2025_06_25_102844_create_begin_mandates_table.php
+++ database/migrations/2025_06_25_102844_create_begin_mandates_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
             $table->decimal('credit', 15, 0)->default(0);
             $table->decimal('law_average', 15, 2)->default(0);
             $table->decimal('law_correction', 15, 2)->default(0);
-            $table->unsignedBigInteger('account_sub_id');
+            $table->unsignedBigInteger('expense_type_id');
             $table->timestamps();
             // $table->softDeletes();
         });
