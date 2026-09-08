e1c6aae7b441028b9af8ebd9ed544ed37b523749
Yeav Mengly <yeavmengly@gmail.com>
2026-09-08 10:02:47 +0700

    fix resolve conflict

--- database/migrations/2026_09_07_162642_create_employees_table.php
+++ database/migrations/2026_09_07_162642_create_employees_table.php
@@ -14,15 +14,9 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('id_number')->nullable()->unique();
-<<<<<<< HEAD:database/migrations/2026_09_07_094017_create_name_list_table.php
             $table->string('account_number', 15)->unique();
             $table->string('name_kh', 50)->unique();
             $table->string('name_latin', 50)->unique();
-=======
-            $table->string('account_number')->unique();
-            $table->string('name_kh')->unique();
-            $table->string('name_latin')->unique();
->>>>>>> 7e99b755e862642799051072924caf2210ea68d0:database/migrations/2026_09_07_162642_create_employees_table.php
             $table->timestamps();
             $table->softDeletes();
         });
